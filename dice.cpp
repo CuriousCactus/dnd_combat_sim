aac82ec56b9e39a5c5bddc347bc76e12878001fc
og113 <oliver.gould@helsinki.fi>
2020-07-27 18:24:09 +0300

    Added a dice_roller using rand(), currently untested

--- dice.cpp
+++ dice.cpp
@@ -3,11 +3,10 @@
 */
 
 #include "dndcs.h"
-#include <cstdlib>
 
 int dice_roll(int number_of_dice, int number_of_faces) {
     int result = 0;
     for (int i=0; i<number_of_dice; i++)
-        result += rand() % number_of_faces;
+        result += 1 + rand() % number_of_faces;
     return result;
 }
