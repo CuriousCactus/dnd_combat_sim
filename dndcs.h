fc01fa5f193aee3c30856b000ad9e2ca13d16026
og113 <oliver.gould@helsinki.fi>
2020-07-27 17:11:08 +0300

    Added very beginnings of the header file, just with libraries so far, and namespace stuff

---
+++ dndcs.h
@@ -0,0 +1,11 @@
+/*
+* header file for dnd combat simulator
+*/
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
