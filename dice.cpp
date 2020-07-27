af8b58fed5eb4504707c1b03e645bd4ad514a83a
og113 <oliver.gould@helsinki.fi>
2020-07-27 23:08:12 +0300

    Added dice_init() function as discussed in the comment to this pull request.

--- dice.cpp
+++ dice.cpp
@@ -4,9 +4,16 @@
 
 #include "dndcs.h"
 
+// rolls a specified number of specified dice
 int dice_roll(int number_of_dice, int number_of_faces) {
     int result = 0;
     for (int i=0; i<number_of_dice; i++)
         result += 1 + rand() % number_of_faces;
     return result;
+}
+
+// initialises random number generator
+// N.B. takes one second to get a new seed
+void dice_init() {
+    srand(time(NULL));
 }
